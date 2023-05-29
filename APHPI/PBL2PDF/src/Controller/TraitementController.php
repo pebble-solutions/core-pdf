3cee25469a7aa9a5705fa789075274b0f1c646b6
PBL <matheo.allain.pro@gmail.com>
2023-05-29 16:56:40 +0200

    Correction de la gestion d'erreurs dans l'API

--- APHPI/PBL2PDF/src/Controller/TraitementController.php
+++ APHPI/PBL2PDF/src/Controller/TraitementController.php
@@ -24,182 +24,202 @@ class TraitementController extends AbstractController
 {
 
     #[Route('/upload', name: 'app_upload', methods:['POST'])]
-    public function uploadFichier(Request $request,MessageBusInterface $bus,EntityManagerInterface $entityManager): Response
+    public function uploadFichier(Request $request, MessageBusInterface $bus, EntityManagerInterface $entityManager): Response
     {
-        $fs = new FileSystem();
-        $file = $request->files->get('Fichier');
-        $header = $request->files->get('Header');
-        $footer = $request->files->get('Footer');
-        $css = $request->files->get('Style');
-        $validExtensions = ['html', 'htm'];
+        try {
+            $fs = new FileSystem();
+            $file = $request->files->get('Fichier');
+            $header = $request->files->get('Header');
+            $footer = $request->files->get('Footer');
+            $css = $request->files->get('Style');
+            $validExtensions = ['html', 'htm'];
 
-        if (!$request->files->has('Fichier')) {
-            return new JsonResponse(['error' =>'Le fichier HTML n\'a pas été transmis.']);
-        }
-        
-        $extensionFile = $file->getClientOriginalExtension();
+            if (!$file instanceof UploadedFile) {
+                throw new \InvalidArgumentException('Le fichier HTML n\'a pas été transmis.');
+            }
 
-        if (!in_array($extensionFile, $validExtensions)) {
-            return new JsonResponse(['error' => 'Le fichier doit être un fichier HTML ou HTM.']);
-        }
+            $extensionFile = $file->getClientOriginalExtension();
 
-        $extensionHeader = $header ? $header->getClientOriginalExtension() : null;
-        if (isset($extensionHeader) && !in_array($extensionHeader, $validExtensions)) {
-            return new JsonResponse(['error' => 'Le fichier de l\'en-tête doit être un fichier HTML ou HTM.']);
-        }
+            if (!in_array($extensionFile, $validExtensions)) {
+                throw new \InvalidArgumentException('Le fichier doit être un fichier HTML ou HTM.');
+            }
 
-        $extensionFooter = $footer ? $footer->getClientOriginalExtension() : null;
-        if (isset($extensionFooter) && !in_array($extensionFooter, $validExtensions)) {
-            return new JsonResponse(['error' => 'Le fichier de pied de page doit être un fichier HTML ou HTM.']);
-        }
+            $extensionHeader = $header ? $header->getClientOriginalExtension() : null;
+            if (isset($extensionHeader) && !in_array($extensionHeader, $validExtensions)) {
+                throw new \InvalidArgumentException('Le fichier de l\'en-tête doit être un fichier HTML ou HTM.');
+            }
 
-        $extensionCss = $css ? $css->getClientOriginalExtension() : null;
-        if (isset($extensionCss) && $extensionCss!="css") {
-            return new JsonResponse(['error' => 'Le fichier de style doit être un fichier CSS']);
-        }
+            $extensionFooter = $footer ? $footer->getClientOriginalExtension() : null;
+            if (isset($extensionFooter) && !in_array($extensionFooter, $validExtensions)) {
+                throw new \InvalidArgumentException('Le fichier de pied de page doit être un fichier HTML ou HTM.');
+            }
 
-        $id= uniqid();
-        $directory= 'uploads/'.$id;
-        $fileName = $id . '.html';
-        $headerName = 'header.html';
-        $footerName = 'footer.html';
-        $cssName = 'style.css';
-        if (!file_exists($directory)) {
-            mkdir($directory, 0777, true);
-        }
-        $file->move(
-            $directory,
-            $fileName
-        );
-        if($header){
-            $header->move(
+            $extensionCss = $css ? $css->getClientOriginalExtension() : null;
+            if (isset($extensionCss) && $extensionCss != "css") {
+                throw new \InvalidArgumentException('Le fichier de style doit être un fichier CSS.');
+            }
+
+            $id = uniqid();
+            $directory = 'uploads/' . $id;
+            $fileName = $id . '.html';
+            $headerName = 'header.html';
+            $footerName = 'footer.html';
+            $cssName = 'style.css';
+
+            if (!file_exists($directory)) {
+                mkdir($directory, 0777, true);
+            }
+
+            $file->move(
                 $directory,
-                $headerName
+                $fileName
             );
-        }else{
-            $fs->copy($this->getParameter('default_file_path'),$directory."/$headerName");
-        }
-        if($footer){
-            $footer->move(
-                $directory,
-                $footerName
-            );
-        }else{
-            $fs->copy($this->getParameter('default_file_path'),$directory."/$footerName");
-        }
-        if($css){
-            $css->move(
-                $directory,
-                $cssName
-            );
-        }else{
-            $fs->copy($this->getParameter('default_file_path'),$directory."/$cssName");
-        }
 
-        $operation = new Operation();
-        $operation->setNom($fileName);
-        $operation->setEtat('Running');
-        $operation->setContenuBase64('');
-        $entityManager->persist($operation);
-        $entityManager->flush();
-        $operationId = $operation->getId();
-        $bus->dispatch(new TraitementFichierMessage($operationId,$id,$fileName,$headerName,$footerName,$cssName));
+            if ($header) {
+                $header->move(
+                    $directory,
+                    $headerName
+                );
+            } else {
+                $fs->copy($this->getParameter('default_file_path'), $directory . "/$headerName");
+            }
 
-        return new JsonResponse(['success' => 'L\'opération a été enregistrée avec succès. Votre pdf va être généré dans quelques instants.']);
+            if ($footer) {
+                $footer->move(
+                    $directory,
+                    $footerName
+                );
+            } else {
+                $fs->copy($this->getParameter('default_file_path'), $directory . "/$footerName");
+            }
+
+            if ($css) {
+                $css->move(
+                    $directory,
+                    $cssName
+                );
+            } else {
+                $fs->copy($this->getParameter('default_file_path'), $directory . "/$cssName");
+            }
+
+            $operation = new Operation();
+            $operation->setNom($fileName);
+            $operation->setEtat('Running');
+            $operation->setContenuBase64('');
+            $entityManager->persist($operation);
+            $entityManager->flush();
+            $operationId = $operation->getId();
+            $bus->dispatch(new TraitementFichierMessage($operationId, $id, $fileName, $headerName, $footerName, $cssName));
+
+            return new JsonResponse(['success' => 'L\'opération a été enregistrée avec succès. Votre PDF sera généré dans quelques instants.']);
+        } catch (\InvalidArgumentException $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Une erreur est survenue lors du traitement du fichier.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
-
-
 
     #[Route('/fichiers', name: 'app_liste_fichiers')]
     public function listeFichiers(): Response
     {
-        $fichiers = scandir($this->getParameter('render_directory'));
-        $fichiers = array_filter($fichiers, function ($fichier) {
-            return $fichier !== '.' && $fichier !== '..';
-        });
-    
-        /*
-        return $this->render('traitement/liste_fichiers.html.twig', [
-            'fichiers' => $fichiers,
-        ]);*/
+        try {
+            $fichiers = scandir($this->getParameter('render_directory'));
+            $fichiers = array_filter($fichiers, function ($fichier) {
+                return $fichier !== '.' && $fichier !== '..';
+            });
 
-        $data = [];
-        foreach ($fichiers as $fichier) {
-            $data[] = [
-                'nom' => $fichier,
-            ];
+            $data = [];
+            foreach ($fichiers as $fichier) {
+                $data[] = [
+                    'nom' => $fichier,
+                ];
+            }
+
+            return new JsonResponse($data);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Une erreur est survenue lors de la récupération de la liste des fichiers.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return new Response(json_encode($data), 200, [
-            'Content-Type' => 'application/json'
-        ]);
     }
-
 
     #[Route('/operations', name: 'app_liste_operations')]
     public function listeOperations(EntityManagerInterface $entityManager): Response
     {
-        $operations = $entityManager->getRepository(Operation::class)->findAll();
-    
-        $data = [];
-        foreach ($operations as $operation) {
-            $data[] = [
-                'id' => $operation->getId(),
-                'fichier' => $operation->getNom().'.pdf',
-                'etat' => $operation->getEtat(),
-            ];
+        try {
+            $operations = $entityManager->getRepository(Operation::class)->findAll();
+
+            $data = [];
+            foreach ($operations as $operation) {
+                $data[] = [
+                    'id' => $operation->getId(),
+                    'fichier' => $operation->getNom() . '.pdf',
+                    'etat' => $operation->getEtat(),
+                ];
+            }
+
+            return new JsonResponse($data);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Une erreur est survenue lors de la récupération de la liste des opérations.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    
-        return new Response(json_encode($data), 200, [
-            'Content-Type' => 'application/json'
-        ]);
     }
-    
+
     #[Route('/fichier/{id}', name: 'app_fichier')]
-    public function afficherFichier(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $operation = $entityManager->getRepository(Operation::class)->find($id);
-        $fichierPath = $this->getParameter('render_directory') . '/' . $operation->getNom() . '.pdf';
-    
-        if (file_exists($fichierPath)) {
-            $data = [
-                'id' => $operation->getId(),
-                'fichier' => $operation->getNom().'.pdf',
-                'contenu' => $operation->getContenuBase64(),
-            ];
-    
-            return new Response(json_encode($data), 200, [
-                'Content-Type' => 'application/json'
-            ]);
-        } else {
-            return new Response('Le fichier demandé n\'existe pas.', Response::HTTP_NOT_FOUND);
-        }
-    }
-    
+public function afficherFichier(EntityManagerInterface $entityManager, int $id): Response
+{
+    $operation = $entityManager->find(Operation::class, $id);
 
-    #[Route('/fichier/del/{id}/', name: 'app_supprimer_fichier', methods: ['DELETE'])]
-    public function supprimerFichier(EntityManagerInterface $entityManager,int $id): Response
-    {
-        $operation = $entityManager->getRepository(Operation::class)->find($id);
-        $fichierPath = $this->getParameter('render_directory') . '/' . $operation->getNom() .'.pdf';
-        if (file_exists($fichierPath)) {
-            unlink($fichierPath);
-    
-            $operation = $entityManager->getRepository(Operation::class)->find($id);
-            $operation->setEtat('Deleted');
-            $operation->setContenuBase64('');
-            $entityManager->persist($operation);
-            $entityManager->flush();
-    
-            $message = 'Le fichier a été supprimé avec succès.';
-            $response = new JsonResponse(['message' => $message], Response::HTTP_OK);
-        } else {
-            $message = 'Une erreur est survenue lors de la suppression du fichier.';
-            $response = new JsonResponse(['message' => $message], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-    
-        return $response;
+    if (!$operation) {
+        return new JsonResponse(['error' => 'L\'opération avec l\'ID spécifié n\'existe pas.'], Response::HTTP_NOT_FOUND);
     }
 
+    $fichierPath = $this->getParameter('render_directory') . '/' . $operation->getNom() . '.pdf';
+
+    if (file_exists($fichierPath)) {
+        $data = [
+            'id' => $operation->getId(),
+            'fichier' => $operation->getNom() . '.pdf',
+            'contenu' => $operation->getContenuBase64(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    $data = [
+        'id' => $operation->getId(),
+        'fichier' => $operation->getNom() . '.pdf',
+        'contenu' => '',
+    ];
+
+    return new JsonResponse($data);
 }
 
+    
+    
+    #[Route('/fichier/del/{id}/', name: 'app_supprimer_fichier', methods: ['DELETE'])]
+    public function supprimerFichier(EntityManagerInterface $entityManager, int $id, Filesystem $filesystem): Response
+    {
+        try {
+            $operation = $entityManager->getRepository(Operation::class)->find($id);
+    
+            if (!$operation) {
+                $message = 'L\'opération avec l\'ID spécifié n\'existe pas.';
+                return new JsonResponse(['error' => $message], Response::HTTP_NOT_FOUND);
+            }
+    
+            $fichierPath = $this->getParameter('render_directory') . '/' . $operation->getNom() . '.pdf';
+    
+            if ($filesystem->exists($fichierPath)) {
+                $filesystem->remove($fichierPath);
+            }
+    
+            $entityManager->remove($operation);
+            $entityManager->flush();
+    
+            $message = 'L\'opération et le fichier ont été supprimés avec succès.';
+            return new JsonResponse(['message' => $message]);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Une erreur est survenue lors de la suppression du fichier.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+
+}
